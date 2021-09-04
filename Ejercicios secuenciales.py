8daeaecfa1b9fd0187fcf3f3aac6961a355d67b2
andresfvb <barandicandres@gmail.com>
2021-09-03 22:08:01 -0500

    Seccion 2 - Punto 1

--- Ejercicios secuenciales.py
+++ Ejercicios secuenciales.py
@@ -5,7 +5,7 @@ Created on Fri Sep  3 19:59:11 2021
 @author: Andres Vasquez
 """
 
-# Calcule el valor de Y indicando paso a paso como llegó al resultado
+# Seccion 1
 
 # punto 1 ---------------------------------------------------------------------
 
@@ -30,3 +30,17 @@ m = pow(z, 2)*3+n
 y = (pow(pow((pow(z+2-n, 2)*m+8/2-30)/2*5-3, 5)+15*3-9/3, 2)-5/4)
 
 print(y)
+
+# Seccion 2
+
+# punto 1 ---------------------------------------------------------------------
+
+presion = float(input('Digite el valor de la Presion: '))
+volumen = float(input('Digite el valor del Volúmen: '))
+temperatura = float(input('Digite el valor de la Temperatura: '))
+masa = (presion*volumen)/(0.37*(temperatura+460))
+
+print("Haga un algoritmo que calcule la masa de la siguiente fórmula: ")
+print("Masa = (presión * volúmen) / (0.37 * (temperatura + 460))")
+print(f"masa = ({presion}*{volumen}/(0.37*{temperatura+460})) = {masa}")
+
