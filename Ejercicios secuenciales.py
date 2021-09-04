85ea37d5556fae3369ed632ca23eaff69d84c246
andresfvb <barandicandres@gmail.com>
2021-09-03 21:22:14 -0500

    Punto 1

--- Ejercicios secuenciales.py
+++ Ejercicios secuenciales.py
@@ -5,3 +5,10 @@ Created on Fri Sep  3 19:59:11 2021
 @author: Andres Vasquez
 """
 
+# Calcule el valor de Y indicando paso a paso como llegó al resultado
+
+# punto 1 ---------------------------------------------------------------------
+
+y = ((pow((5+2-5), 2))*5+8/2-30)/2*5-3
+
+print(f"y = ((5+2-5)^2*5+8/2-30)/2*5-3 = {y}")
